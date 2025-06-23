8423ee4bc38ad6816e5c6bd1159125a5a1b4af05
DDACUNHAGOMES <daniel.dacunhagomes@ynov.com>
2025-06-23 11:10:31 +0200

    aaaaaaaaaaaaa

--- scripts/setup_routeur.sh
+++ scripts/setup_routeur.sh
@@ -7,9 +7,6 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo apt-get update
 sudo apt-get install -y frr openvswitch-switch
 
-# Configurer interfaces réseau (par défaut Vagrant les configure, ici juste vérif)
-ip addr add 192.168.10.11/24 dev eth0
-ip addr add 192.168.20.1/24 dev eth1  # Y=20 pour r1, 30 pour r2
 
 # Démarrer et configurer FRR avec OSPF
 cat <<EOF > /etc/frr/frr.conf
--- scripts/setup_routeur2.sh
+++ scripts/setup_routeur2.sh
@@ -7,9 +7,7 @@ sudo sysctl -w net.ipv4.ip_forward=1
 sudo apt-get update
 sudo apt-get install -y frr openvswitch-switch
 
-# Configurer interfaces réseau (par défaut Vagrant les configure, ici juste vérif)
-ip addr add 192.168.10.12/24 dev eth0
-ip addr add 192.168.30.1/24 dev eth1  # Y=20 pour r1, 30 pour r2
+
 
 # Démarrer et configurer FRR avec OSPF
 cat <<EOF > /etc/frr/frr.conf
